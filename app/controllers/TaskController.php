ef271f3e9180ef3cceb1e879bc9ed88eca6e321e
IsmaelG <ismaelgn89@gmail.com>
2026-05-15 12:31:27 +0200

    feat: add validation in TaskController createAction

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -4,6 +4,14 @@ class TaskController extends Controller
 {
     public function createAction()
     {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $required = ['name', 'description', 'start_date', 'user'];
 
+            foreach ($required as $field) {
+                if (empty($_POST[$field])) {
+                    $errors[] = $field . ' is required';
+                }
+            }
+        }
     }
 }
