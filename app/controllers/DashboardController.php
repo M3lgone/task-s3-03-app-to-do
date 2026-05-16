8c0e656e30094d71994a2936c81ef23a41dfdb6d
IsmaelG <ismaelgn89@gmail.com>
2026-05-16 18:49:57 +0200

    feat: add DashboardController with getTasks

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -1,9 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 class DashboardController extends Controller
 {
+    private TaskModel $taskModel;
+
+    public function __construct()
+    {
+        $this->taskModel = new TaskModel();
+    }
     public function indexAction()
     {
-
+        $this->view->tasks = $this->taskModel->getTasks();
     }
 }
--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 class TaskController extends Controller
 {
     public function createAction()
