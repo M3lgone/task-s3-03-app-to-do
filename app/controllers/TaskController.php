bef552a5f3e6cc82e1abf9be97ff7ae5e4b1a852
IsmaelG <ismaelgn89@gmail.com>
2026-05-20 15:56:28 +0200

    feat: complete editAction and edit form view

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -40,6 +40,30 @@ class TaskController extends Controller
     public function editAction()
     {
         $id = (int) $this->_getParam('id');
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $required = ['name', 'description', 'start_date', 'user'];
+
+            foreach ($required as $field) {
+                if (empty($_POST[$field])) {
+                    $errors[] = $field . ' is required';
+                }
+            }
+            if (!empty($errors)) {
+                $this->view->errors = $errors;
+            } else {
+                $this->taskModel->updateTask(
+                    $id,
+                    $_POST['name'],
+                    $_POST['description'],
+                    $_POST['start_date'],
+                    $_POST['finish_date'],
+                    $_POST['user']
+                );
+                header('Location: ' . WEB_ROOT . '/dashboard');
+                exit();
+            }
+        }
         $this->view->task = $this->taskModel->getTaskById($id);
     }
 }
