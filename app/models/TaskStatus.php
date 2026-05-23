39fa18133cffe07555039be78cf49111960d20cf
IsmaelG <ismaelgn89@gmail.com>
2026-05-23 11:24:55 +0200

    fix: change in progress

--- app/models/TaskStatus.php
+++ app/models/TaskStatus.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 enum TaskStatus: string
 {
-    case PENDING = 'pending';
-    case IN_PROGRESS = 'in_progress';
+    case PENDING = 'Pending';
+    case IN_PROGRESS = 'In progress';
     case DONE = 'Done';
 }
